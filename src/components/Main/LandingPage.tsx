e7155d64aa1f62aefe7fd0bf7097b036db22ba99
solorankYT <43119617+solorankYT@users.noreply.github.com>
2024-08-15 21:33:57 +0800

    updated
    add feauture transition

--- src/components/Main/LandingPage.tsx
+++ src/components/Main/LandingPage.tsx
@@ -7,12 +7,20 @@ return(
 
 <Container className="container-landing  d-flex justify-content-center align-items-center fadeInUp">
 <Row className="d-flex  align-items-center">
+    
     <Col md={8} className="p-5">
         <h1 className="text-white">Hi! I'm <span className="text-color-primary">Mark Funa</span></h1>
-        <h2 className="text-color-light">Front-end developer</h2>
+        <h3 className="text-color-light typewriter">Front-end developer.</h3>
+       
+
         <Link to="contact" className="link-design">
         <div className="mt-4 button ">Contact me</div>
         </Link>
+        <a className="link-design" href="./MarkFunaResume.docx" download>
+        <div className="mt-4">Download CV</div>
+        </a> 
+        
+
     </Col>
   
     <Col md={4} className="p-5 hide-on-small">
