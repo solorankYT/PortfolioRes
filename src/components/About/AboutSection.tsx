8aaf7a035f74b3389134135dca6a09c08bb6e39a
solorankYT <43119617+solorankYT@users.noreply.github.com>
2024-08-19 18:12:46 +0800

    update contact icon
    contact icon has now hover effect that will change the color to green when they hover the contact icon

--- src/components/About/AboutSection.tsx
+++ src/components/About/AboutSection.tsx
@@ -4,7 +4,6 @@ import SkillsSection from "../Skills/SkillsSection";
 function AboutSection(){
     return(
       <>
-      
       <Container className="containerAbout fadeInUp" id="AboutPage" >
       <h1 className="text-color-primary">About</h1>
       <p className="AboutText text-color-secondary">
@@ -12,32 +11,28 @@ function AboutSection(){
       <span className="text-color-primary"> I enjoy working on creating interactive and user-friendly web applications, and I'm continuously learning to deliver better and more visually appealing projects. 
       </span> While I have much to learn, I'm committed to growing in this field and staying updated with the latest trends and technologies. 
       My goal is to keep improving and, hopefully, contribute to projects that meet or exceed expectations.
-
- 
-        </p>
-
-       
+      </p>   
         <Row className="text-center" style={{margin: '5rem'}}>
             <h1 className="text-color-primary">What I Offer</h1>
           <Col md={4} className="d-flex justify-content-center">
-      <Card className="background-color"style={{ width: '18rem' }}>
-          <Card.Img style={{ width: '10em', margin: '0 auto' , padding: '10px'}} variant="top" src="./webdevelopment.svg" />
+      <Card className="background-color"style={{ width: '350px' }}>
+          <Card.Img style={{  width: '250px',height:'250px', margin: '0 auto' , padding: '10px'}} variant="top" src="./webdevelopment.svg" />
         <Card.Body>
           <Card.Title className="text-white">Web Development</Card.Title>    
         </Card.Body>
       </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center">
-          <Card className="background-color"style={{ width: '18rem' }}>
-          <Card.Img style={{ width: '10em', margin: '0 auto' , padding: '10px'}} variant="top" src="./webdesign.svg" />
+          <Card className="background-color"style={{ width: '350px' }}>
+          <Card.Img style={{  width: '250px',height:'250px', margin: '0 auto' , padding: '10px'}} variant="top" src="./webdesign.svg" />
         <Card.Body>
           <Card.Title className="text-white">Web Design</Card.Title>    
         </Card.Body>
       </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center">
-          <Card className="background-color"style={{ width: '18rem' }}>
-          <Card.Img style={{ width: '10em', margin: '0 auto' , padding: '10px'}} variant="top" src="./photography.svg" />
+          <Card className="background-color"style={{ width: '350px' }}>
+          <Card.Img style={{ width: '250px',height:'250px', margin: '0 auto' , padding: '10px'}} variant="top" src="./photography.svg" />
         <Card.Body>
           <Card.Title className="text-white">Photography</Card.Title>    
         </Card.Body>
