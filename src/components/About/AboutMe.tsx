8fb2d140664060509a6e6c1594e2c7a38ad4f412
solorankYT <43119617+solorankYT@users.noreply.github.com>
2024-10-09 16:51:25 +0800

    asd

--- src/components/About/AboutMe.tsx
+++ src/components/About/AboutMe.tsx
@@ -8,7 +8,7 @@ function AboutMe(){
         <h1 className="text-white">About me</h1>
         <p className="AboutText text-color-secondary">
         an aspiring Front-End Developer with a growing interest in web development. I'm just starting out, but I'm eager to learn and improve my skills. 
-        <span className="text-color-primary"> I enjoy working on creating interactive and user-friendly web applications, and I'm continuously learning to deliver better and more visually appealing projects. 
+        <span className="text-highlight"> I enjoy working on creating interactive and user-friendly web applications, and I'm continuously learning to deliver better and more visually appealing projects. 
         </span> While I have much to learn, I'm committed to growing in this field and staying updated with the latest trends and technologies. 
         My goal is to keep improving and, hopefully, contribute to projects that meet or exceed expectations.
         </p>   
